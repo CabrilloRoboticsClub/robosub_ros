1c464a14b6140f3d28b5975543af8c5fda95f305
steph1111 <stephanielh1111@gmail.com>
2024-10-19 22:20:32 -0700

    flyng robot

--- src/lazershark_sim/launch/lazershark_world.launch.py
+++ src/lazershark_sim/launch/lazershark_world.launch.py
@@ -75,7 +75,7 @@ def generate_launch_description():
         ),
         launch_arguments={
             "gz_args": "-v4 -s -r "
-            f'{Path(pkg_project_gazebo) / "worlds" / "iris_runway.sdf"}'
+            f'{Path(pkg_lazershark_sim) / "worlds" / "waterworld.sdf"}'
         }.items(),
     )
 
@@ -87,21 +87,21 @@ def generate_launch_description():
     )
 
     # RViz.
-    rviz = Node(
-        package="rviz2",
-        executable="rviz2",
-        arguments=["-d", f'{Path(pkg_project_bringup) / "rviz" / "iris.rviz"}'],
-        condition=IfCondition(LaunchConfiguration("rviz")),
-    )
+    # rviz = Node(
+    #     package="rviz2",
+    #     executable="rviz2",
+    #     arguments=["-d", f'{Path(pkg_project_bringup) / "rviz" / "iris.rviz"}'],
+    #     condition=IfCondition(LaunchConfiguration("rviz")),
+    # )
 
     return LaunchDescription(
         [
-            DeclareLaunchArgument(
-                "rviz", default_value="true", description="Open RViz."
-            ),
+            # DeclareLaunchArgument(
+            #     "rviz", default_value="true", description="Open RViz."
+            # ),
             gz_sim_server,
             gz_sim_gui,
             lazer_shark,
-            rviz,
+            # rviz,
         ]
     )

376a05aaf4345d34d80f0bd4bc8981012e58ab04
steph1111 <stephanielh1111@gmail.com>
2024-11-02 14:59:23 -0700

    Unused libaries in launch files for sim

--- src/lazershark_sim/launch/lazershark_auv.launch.py
+++ src/lazershark_sim/launch/lazershark_auv.launch.py
@@ -4,19 +4,12 @@ from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.actions import IncludeLaunchDescription
 from launch.actions import RegisterEventHandler
-
 from launch.conditions import IfCondition
-
 from launch.event_handlers import OnProcessStart
-
-from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
-from launch.substitutions import PathJoinSubstitution
 
 from launch_ros.actions import Node
-from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
--- src/lazershark_sim/launch/lazershark_world.launch.py
+++ src/lazershark_sim/launch/lazershark_world.launch.py
@@ -1,18 +1,13 @@
 from pathlib import Path
-import subprocess
 
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch.actions import RegisterEventHandler
 from launch.actions import IncludeLaunchDescription, ExecuteProcess
-from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
 from launch.substitutions import PathJoinSubstitution
 from launch.event_handlers import OnShutdown
-
-from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
 

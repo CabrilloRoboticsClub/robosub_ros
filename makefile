2dc463839013f6dcc07d1939d79c05a5bbd07a2e
steph1111 <stephanielh1111@gmail.com>
2024-11-02 15:12:54 -0700

    One launch file

--- makefile
+++ makefile
@@ -5,7 +5,7 @@ colcon-build:
 	colcon build --allow-overriding sdformat_urdf
 
 sim: colcon-build
-	. ./install/setup.sh && ros2 launch lazershark_sim lazershark_world.launch.py
+	. ./install/setup.sh && ros2 launch lazershark_sim lazershark_sim.launch.py
 
 clean:
 	rm -rf build/ log/ install/
--- src/lazershark_sim/launch/lazershark_sim.launch.py
+++ src/lazershark_sim/launch/lazershark_sim.launch.py
@@ -1,26 +1,27 @@
+from pathlib import Path
 import os
 
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.actions import RegisterEventHandler
+from launch.actions import DeclareLaunchArgument, RegisterEventHandler, IncludeLaunchDescription, ExecuteProcess
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.event_handlers import OnShutdown, OnProcessStart
 from launch.conditions import IfCondition
-from launch.event_handlers import OnProcessStart
 from launch.substitutions import LaunchConfiguration
-
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
     """
-    Generate a launch description for a lazershark.
+    Generate a launch description for lazershark_sim gazebo simulator.
+    Launch with: `ros2 launch lazershark_sim lazershark_sim.launch.py`
     """
+    # Get packages
+    pkg_ros_gz_sim = get_package_share_directory("ros_gz_sim")
     pkg_lazershark_sim = get_package_share_directory("lazershark_sim")
 
-    # Robot description.
-
-    # Ensure `SDF_PATH` is populated as `sdformat_urdf`` uses this rather
+    # Ensure `SDF_PATH` is populated as `sdformat_urdf` uses this rather
     # than `GZ_SIM_RESOURCE_PATH` to locate resources.
     if "GZ_SIM_RESOURCE_PATH" in os.environ:
         gz_sim_resource_path = os.environ["GZ_SIM_RESOURCE_PATH"]
@@ -31,14 +32,12 @@ def generate_launch_description():
         else:
             os.environ["SDF_PATH"] = gz_sim_resource_path
 
-    # Load SDF file.
-    # TODO: Change to sdf
+    # Get robot sdf file
     sdf_file = os.path.join(
         pkg_lazershark_sim, "robot", "lazershark_sim.sdf"
     )
     with open(sdf_file, "r") as infp:
         robot_desc = infp.read()
-        # print(robot_desc)
 
     # Publish /tf and /tf_static.
     robot_state_publisher = Node(
@@ -51,7 +50,8 @@ def generate_launch_description():
             {"frame_prefix": ""},
         ],
     )
-    # Bridge.
+
+    # Bridge between ros and gazebo
     bridge = Node(
         package="ros_gz_bridge",
         executable="parameter_bridge",
@@ -60,29 +60,9 @@ def generate_launch_description():
                 "config_file": os.path.join(
                     pkg_lazershark_sim, "config", "ros_gz_bridge.yaml"
                 ),
-                # "qos_overrides./tf_static.publisher.durability": "transient_local",
             }
         ],
-        # output="screen",
     )
-
-    # Transform - use if the model includes "gz::sim::systems::PosePublisher"
-    #             and a filter is required.
-    # topic_tools_tf = Node(
-    #     package="topic_tools",
-    #     executable="transform",
-    #     arguments=[
-    #         "/gz/tf",
-    #         "/tf",
-    #         "tf2_msgs/msg/TFMessage",
-    #         "tf2_msgs.msg.TFMessage(transforms=[x for x in m.transforms if x.header.frame_id == 'odom'])",
-    #         "--import",
-    #         "tf2_msgs",
-    #         "geometry_msgs",
-    #     ],
-    #     output="screen",
-    #     respawn=True,
-    # )
 
     # Relay - use instead of transform when Gazebo is only publishing odom -> base_link
     topic_tools_tf = Node(
@@ -95,6 +75,39 @@ def generate_launch_description():
         output="screen",
         respawn=False,
         condition=IfCondition(LaunchConfiguration("use_gz_tf")),
+    )
+
+    # Gazebo server and gui
+    gz_sim_server = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            f'{Path(pkg_ros_gz_sim) / "launch" / "gz_sim.launch.py"}'
+        ),
+        launch_arguments={
+            "gz_args": "-v4 -s -r "
+            f'{Path(pkg_lazershark_sim) / "worlds" / "waterworld.sdf"}'
+        }.items(),
+    )
+
+    gz_sim_gui = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            f'{Path(pkg_ros_gz_sim) / "launch" / "gz_sim.launch.py"}'
+        ),
+        launch_arguments={"gz_args": "-v4 -g"}.items(),
+    )
+
+    # RViz.
+    # rviz = Node(
+    #     package="rviz2",
+    #     executable="rviz2",
+    #     arguments=["-d", f'{Path(pkg_project_bringup) / "rviz" / "iris.rviz"}'],
+    #     condition=IfCondition(LaunchConfiguration("rviz")),
+    # )
+    
+    # Kill gazebo on quit
+    die_gazebo_die = RegisterEventHandler(
+        OnShutdown(
+            on_shutdown=lambda event, context: ExecuteProcess(cmd=["pkill " " -9 " " -f " "\"gz sim\""], shell=True)
+        )
     )
 
     return LaunchDescription(
@@ -112,5 +125,12 @@ def generate_launch_description():
                     ]
                 )
             ),
+            # DeclareLaunchArgument(
+            #     "rviz", default_value="true", description="Open RViz."
+            # ),
+            gz_sim_server,
+            gz_sim_gui,
+            die_gazebo_die,
+            # rviz,
         ]
     )

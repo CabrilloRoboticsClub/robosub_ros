cb0e097929290eb4d6f3ab7a6fc7b82779748278
steph1111 <stephanielh1111@gmail.com>
2024-11-25 11:04:32 -0800

    Data files

--- src/lazershark/setup.py
+++ src/lazershark/setup.py
@@ -7,12 +7,18 @@ package_name = 'lazershark'
 setup(
     name=package_name,
     version='0.0.1',
-    packages=["gazebo_test", "lazershark_sim"],
+    packages=["gazebo_test"],
     data_files=[
         (os.path.join('share', package_name), ['package.xml']),
         (os.path.join('share', package_name, 'params'), glob(os.path.join('params', '*'))),
         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
-        (os.path.join('share', package_name, 'lazershark_sim'), glob(os.path.join('lazershark_sim', '*'))),
+        (os.path.join('share', package_name, 'lazershark_sim', 'robot'), glob(os.path.join('lazershark_sim', 'robot', '*'))), 
+        (os.path.join('share', package_name, 'lazershark_sim', 'worlds'), glob(os.path.join('lazershark_sim', 'worlds', '*'))),
+        (os.path.join('share', package_name, 'lazershark_sim', 'config'), glob(os.path.join('lazershark_sim', 'config', '*'))),
+        # (os.path.join('share', package_name, 'lazershark_sim'), glob(os.path.join('lazershark_sim', '*')))
+        # (os.path.join('share', package_name, 'lazershark_sim', 'robot', 'meshes'), glob(os.path.join('lazershark_sim', 'robot', 'meshes', '*'))),
+        # (os.path.join('share', package_name, 'lazershark_sim', 'worlds'), glob(os.path.join('lazershark_sim', 'worlds', '*'))),
+        # (os.path.join('share', package_name, 'lazershark_sim', 'config'), glob(os.path.join('lazershark_sim', 'config', '*'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
